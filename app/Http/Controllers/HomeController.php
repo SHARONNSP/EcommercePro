12e4b393179a00c75e82577886e7080faaed580f
sharonnsp96 <sharonnsp@appengage.asia>
2023-12-01 00:16:15 +0800

    Add Dashboard Cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,24 @@ class HomeController extends Controller
 
         if($usertype == '1')
         {
-            return view('admin.home');
+            $total_product=product::all()->count();
+            $total_order=order::all()->count();
+            $total_user=user::all()->count();
+            $order=order::all();
+
+            $total_revenue=0;
+
+            foreach($order as $order)
+            {
+                $total_revenue=$total_revenue + $order->price;
+            }
+
+            $order_processing = order::where('delivery_status', '=','processing')->get()->count();
+            $order_delivered= order::where('delivery_status', '=','delivered')->get()->count();
+
+
+
+            return view('admin.home', compact('total_product','total_order','total_user','total_revenue','order_processing','order_delivered','order_delivered'));
         }
 
         else
